aac707caf82da9100a3b766ded296aba5782fd8a
Darius Sobczak <mail@dsobczak.de>
2021-08-16 11:49:22 +0200

    upgrade api

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export {Api, Route, EMethod, KeyMatch} from 'https://deno.land/x/deno_api_server@v0.2.3/mod.ts';
-export type {IRoute} from 'https://deno.land/x/deno_api_server@v0.2.3/mod.ts';
+export {Api, Route, EMethod, KeyMatch} from 'https://deno.land/x/deno_api_server@v0.2.6/mod.ts';
+export type {IRoute} from 'https://deno.land/x/deno_api_server@v0.2.6/mod.ts';
